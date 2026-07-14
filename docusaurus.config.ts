6461ead3b7b72ce64bfe882254a9b752c50b20c8
James Cacchione <james@planufacture.tech>
2026-07-14 14:11:32 +0100

    update for cname

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://planufacture.github.io',
+  url: 'https://docs.planufacture.tech',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
